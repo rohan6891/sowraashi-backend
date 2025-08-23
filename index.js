48bbf2069f692c8da4957f1b46098292b7cddae4
rohan6891 <rohan5144320@gmail.com>
2025-08-23 23:03:47 +0530

    added module to package.json

--- index.js
+++ index.js
@@ -127,9 +127,10 @@ app.use((err, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
